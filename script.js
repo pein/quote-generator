69029f10654167ba0c1db36a371d64e0cdb729f6
Ilyes Houha <mi.houha@gmail.com>
2020-11-28 09:15:49 +0100

    Proxy to fix CORS error

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Get Quote FROM API
 
 async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const APIUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
-    const res = await fetch(APIUrl);
+    const res = await fetch(proxyUrl + APIUrl);
     const data = await res.json();
     console.log(data);
   } catch (error) {
